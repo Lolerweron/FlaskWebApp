405c381e308bbf8e746989f139e7b61388cd1377
n/a <n/a>
2018-11-30 17:58:52 +0100

    FF

--- app/routes.py
+++ app/routes.py
@@ -10,3 +10,12 @@ def index():
 def getUser(name):
     return render_template('user.html', name=name)
 
+@app.route('/galeria')
+def f():
+    obrazki = [
+        'http://www.dekomozaik.pl/userdata/gfx/13a5beec12de0bd6f358e3003b87e21e.jpg',
+        'http://maxfruit.pl/226-large_default/pomarancza.jpg',
+        'https://www.zajadam.pl/wp-content/uploads/2015/02/Pomarancza.png',
+        'https://res.cloudinary.com/dj484tw6k/f_auto,q_auto,c_pad,b_white,w_360,h_360/v1499889192/be/82135.jpg'
+    ]
+    return render_template('galeria.html', obrazki=obrazki)
